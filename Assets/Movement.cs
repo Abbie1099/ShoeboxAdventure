27a78c6b083b700f62179d363f74a7345c8fd32c
Abani Das <nn.abani.pop@gmail.com>
2021-03-02 00:05:18 +0530

    Add project files.

---
+++ Assets/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float playerspeed;
+    private float horizontal;
+    private float vertical;
+    public Text txt;
+    public AudioClip coinsound;
+    public AudioSource ad;
+    int count = 0;
+    public enemy en;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0.0f, -horizontal, 0.0f) * playerspeed * Time.deltaTime);
+        transform.Translate(new Vector3(0.0f, 0.0f, vertical) * playerspeed * Time.deltaTime);
+
+    }
+
+    public void loadscene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        int temp = scene.buildIndex;
+        SceneManager.LoadScene(temp + 1);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GameOver");
+        }
+        if (other.tag == "Coins")
+        {
+            Destroy(other.gameObject);
+            //ad.PlayOneShot(coinsound);
+            ad.Play();
+            count++;
+            txt.text = "Score:" + count;
+        }
+        if(count >=3)
+        {
+            en.enabled = true;
+        }
+        if (count >= 6)
+        {
+            loadscene();
+        }
+
+    }
+}
---
+++ Assets/enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    public NavMeshAgent nv;
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nv.destination = player.gameObject.transform.position;
+    }
+
+}
